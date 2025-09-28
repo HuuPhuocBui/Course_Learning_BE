3aef115d4e7ba9205c1c75d90cfcc3a811ea592c
HuuPhuocBui <135402077+HuuPhuocBui@users.noreply.github.com>
2025-09-28 16:30:52 +0700

    first commit

---
+++ src/main/java/com/example/course_learning_be/CourseLearningBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_learning_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseLearningBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseLearningBeApplication.class, args);
+	}
+
+}
